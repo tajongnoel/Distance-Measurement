0168a29dc61292ff81b9ae2aa6c040cbbd09af08
tajongnoel <payzynoel@gmail.com>
2024-06-01 06:21:41 +0800

    Add files via upload

---
+++ Ultrasonic/ultrasonic.ino
@@ -0,0 +1,58 @@
+#include <LiquidCrystal.h>
+
+#define trigPin 8
+#define echoPin 9
+
+
+//                BS E  D4 D5 D6 D7
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+int pos = 0;    // variable to store the servo position 
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+   
+  lcd.begin(20, 4);
+
+  lcd.setCursor(0, 1);
+  delay(400);
+  lcd.clear();
+  delay(1000);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 24) {  // This is where the LED On/Off happens
+    
+
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("Hurdle Detected");
+  Serial.println(" Hurdle Detected");
+ 
+  delay(500);
+  lcd.clear();
+  
+  delay(300);
+}
+ 
+  lcd.setCursor(1, 0);
+  lcd.print("Distance= ");
+ 
+  lcd.print(distance);
+  lcd.print("cm");
+  
+    Serial.print(distance);
+    Serial.println(" cm");
+ 
+  delay(500);
+}
